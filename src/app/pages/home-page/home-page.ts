56275d6d85d206679fd2e9a3f114d77a871db26c
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-08-18 13:34:19 +0530

    Done implement offer component.

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MainHeader} from './inner-items/main-header/main-header';
 import {OptionComponent} from './inner-items/option-component/option-component';
+import {OffersComponent} from './inner-items/offers-component/offers-component';
 
 @Component({
   selector: 'app-home-page',
   imports: [
     MainHeader,
-    OptionComponent
+    OptionComponent,
+    OffersComponent
   ],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
