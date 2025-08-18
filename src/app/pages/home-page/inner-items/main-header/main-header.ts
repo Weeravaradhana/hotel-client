ba95228520f809ad7e96a49b07db25bf66cb556a
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-08-18 11:52:11 +0530

    Button responsive

---
+++ src/app/pages/home-page/inner-items/main-header/main-header.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-main-header',
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './main-header.html',
+  styleUrl: './main-header.scss'
+})
+export class MainHeader {
+
+}
